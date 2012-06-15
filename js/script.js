ef69384cb44b59ef47d17078ff370583e4d13100
David Harrison <david.harrison@stress-free.co.nz>
2012-06-15 16:03:58 +1200

    Added the Javascript file and continued work on the CSS.

---
+++ js/script.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+Drupal.behaviors.aplsHomepage = {
+  attach: function (context, settings) {
+        $('#block-user-login div.content').addClass('element-invisible');
+        $('#block-user-login h2').click(function(e) {
+            $('#block-user-login h2').toggleClass('selected');
+           $('#block-user-login div.content').toggleClass('element-invisible');
+        });
+        
+        function nextSlide(){
+            nextIndex = currentIndex + 1;
+            if (nextIndex> $('body.node-type-homepage div.field-type-image div.field-item').length) {
+                nextIndex =1;
+            }
+            $("'body.node-type-homepage div.field-type-image div.field-item:nth-child("+nextIndex+")'").addClass('show').animate({opacity: 1.0}, fadeDuration);
+            $("'body.node-type-homepage div.field-type-image div.image-counter li:nth-child("+nextIndex+")'").children('a').animate({opacity: 1.0}, fadeDuration);
+            $("'body.node-type-homepage div.field-type-image div.field-item:nth-child("+currentIndex+")'").animate({opacity: 0.0}, fadeDuration).removeClass('show');
+            $("'body.node-type-homepage div.field-type-image div.image-counter li:nth-child("+currentIndex+")'").children('a').animate({opacity: 0.0}, fadeDuration);
+            currentIndex = nextIndex;
+        }
+        
+        var fadeDuration = 2000;
+        var slideDuration = 15000;
+        var currentIndex = 1;
+        var nextIndex = 1;
+        
+        $('body.node-type-homepage div.field-type-image').append('<div class="image-counter"><ul></ul></div>');
+        $('body.node-type-homepage div.field-type-image div.field-item').each(function(index, value) {
+            $('body.node-type-homepage div.field-type-image div.image-counter ul').append('<li><a><!-- --></a></li>');
+        });
+                        
+        $('body.node-type-homepage div.field-type-image div.field-item').css({opacity: 0.0});
+        $('body.node-type-homepage div.field-type-image div.image-counter li a').css({opacity: 0.0});
+        $("'body.node-type-homepage div.field-type-image div.field-item:nth-child("+nextIndex+")'").addClass('show').animate({opacity: 1.0}, fadeDuration);
+        $("'body.node-type-homepage div.field-type-image div.image-counter li:nth-child("+nextIndex+")'").children('a').animate({opacity: 1.0}, fadeDuration);
+        var timer = setInterval(nextSlide,slideDuration);
+                
+      }
+};
+
+}(jQuery));
