d2222a8596906f0e58c36d7f2daa0963b1bd2338
David Harrison <david.harrison@stress-free.co.nz>
2012-07-18 17:20:16 +1200

    Added favicons and worked on mobile themes.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -3,6 +3,9 @@
 <!--[if gte IE 9]><!--><html><!--<![endif]-->
 <head>
   <?php print $head; ?>
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php print $GLOBALS['base_path'] . drupal_get_path( 'theme', variable_get('theme_default', '0') ) .'/favicon/apple-touch-icon-114x114-precomposed.png' ?>">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php print $GLOBALS['base_path'] . drupal_get_path( 'theme', variable_get('theme_default', '0') ) .'/favicon/apple-touch-icon-72x72-precomposed.png' ?>">
+  <link rel="apple-touch-icon-precomposed" href="<?php print $GLOBALS['base_path'] . drupal_get_path( 'theme', variable_get('theme_default', '0') ) .'/favicon/apple-touch-icon-57x57-precomposed.png' ?>">
   <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
